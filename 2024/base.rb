320a9c0f9c1a9fcae7417053fec17fea8c10ff7d
Jeff Manian <jeffrey.manian@gmail.com>
2024-12-03 09:54:56 -0500

    fix file loading

--- 2024/base.rb
+++ 2024/base.rb
@@ -4,11 +4,17 @@ module Base
     load_data
   end
 
-  def file
+  def filename
     if @test_mode
       "sample.txt"
     else
       "data.txt"
     end
+  end
+
+  # Allow running the script from any working directory by building
+  # the full file path relative to the class's source location.
+  def file
+    File.join(File.dirname(Object.const_source_location(self.class.name).first), filename)
   end
 end
